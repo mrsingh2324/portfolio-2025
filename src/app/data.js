1687d9730f95ec32d25060f7be50cf94e5a48285
Satyam Singh <satyamsingh@Satyams-MacBook-Air.local>
2025-01-26 11:21:24 +0530

    portfolio

---
+++ src/app/data.js
@@ -0,0 +1,73 @@
+
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Metaverse Game",
+    description: "A multiplayer 2d-metaverse game",
+    date: "2025-01-25",
+    demoLink: "https://github.com/mrsingh2324/metaverse-game",
+  },
+  {
+    id: 2,
+    name: "Grabit - Go and Grab it",
+    description: "A food delivery app",
+    date: "2024-04-28",
+    demoLink: "https://github.com/mrsingh2324/grabit",
+  },
+  {
+    id: 3,
+    name: "Face Detection",
+    description: "A face detection app",
+    date: "2022-05-11",
+    demoLink: "https://github.com/mrsingh2324/Face-Detection",
+    
+  },
+  {
+    id: 4,
+    name: "News App",
+    description: "A real-time new application.",
+    date: "2024-05-29",
+    demoLink: "https://github.com/mrsingh2324/NewsApp",
+  },
+  {
+    id: 5,
+    name: "Typing-Speed",
+    description: "A Monkeytype Clone🚀",
+    date: "2023-06-30",
+    demoLink: "https://github.com/mrsingh2324/Typing-Speed-Checker",
+    
+  },
+  
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/mrsingh2324",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/mrsatyamsingh",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://www.x.com/mr_singh2324",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/Resume-Satyam_Singh .pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
